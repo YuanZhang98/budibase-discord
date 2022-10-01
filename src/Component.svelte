d4fcfc65379f21baa4e3a5a2958eb5d9c29ba6d3
Yuan Zhang <mitchellcheung1@yahoo.com>
2022-10-01 15:36:06 +0100

    Create Discord widget component

---
+++ src/Component.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { getContext } from "svelte"
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+  export let discordId = '';
+  let isValid;
+  let url;
+  $: {
+    url = `https://discord.com/widget?id=${discordId}&theme=dark`;
+    isValid = /[0-9]+/.test(discordId);
+  }
+</script>
+
+<div use:styleable={$component.styles}>
+  {#if isValid}
+    <iframe src={url} width="350" height="500" allowTransparency="true" frameBorder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  {:else}
+    <li>Error - Please put down a valid Server ID</li>
+  {/if}
+</div>
